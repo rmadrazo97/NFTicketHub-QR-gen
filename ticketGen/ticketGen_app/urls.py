250faf3c1a0d6be19fc8fc14eb2eb774544cba7d
Alex Madrazo <alex.madrazo@bgamestudios.com>
2023-03-16 12:32:42 +0100

    added ability to create events

--- ticketGen/ticketGen_app/urls.py
+++ ticketGen/ticketGen_app/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("generate-ticket/", views.generate_qr_ticket_view, name="generate-ticket"),
+    path("create-event/", views.create_event, name="create_event"), 
 ]
--- ticketGen/ticketGen_app/views.py
+++ ticketGen/ticketGen_app/views.py
@@ -1,8 +1,48 @@
-from django.shortcuts import render
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponseBadRequest
+from django.views.decorators.csrf import csrf_exempt
 from .ticketGenerator_utils import main as generate_qr_ticket
+import json
 
 def generate_qr_ticket_view(request):
     qr_code_url = generate_qr_ticket()
     response = {"qr_code_url": qr_code_url}
     return JsonResponse(response)
+
+@csrf_exempt
+def create_event(request):
+    if request.method == 'POST':
+        try:
+            data = json.loads(request.body)
+
+            event_name = data.get('event_name')
+            event_description = data.get('event_description')
+            event_sections = data.get('event_sections')
+
+            # Validate and process the event_sections array
+            processed_tickets = []
+            for ticket in event_sections:
+                name = ticket.get('name')
+                price = ticket.get('price')
+                available_seats = ticket.get('available_seats')
+
+                # Perform validation and processing for each ticket, e.g., save to the database
+                processed_ticket = {
+                    "name": name,
+                    "price": price,
+                    "available_seats": available_seats
+                }
+                processed_tickets.append(processed_ticket)
+
+            response = {
+                "event_name": event_name,
+                "event_description": event_description,
+                "event_sections": processed_tickets,
+                "status": "success",
+            }
+            return JsonResponse(response)
+
+        except Exception as e:
+            return HttpResponseBadRequest(str(e))
+
+    else:
+        return HttpResponseBadRequest("Invalid request method")
